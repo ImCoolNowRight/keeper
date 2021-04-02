b3e36dcfe32cdf7cb23bfbe4c1a16e2ed0aa987a
David Richards, Jr <dbarichardsjr@gmail.com>
2021-04-02 19:40:44 -0400

    Make CreateArea collapsible; styling

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,14 @@
 import React, {useState} from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
     const [note, setNote] = useState({
         title: "",
         content: ""
     });
+    const [active, setActive] = useState(false);
 
     function handleChange(event) {
         const {name, value} = event.target;
@@ -25,12 +29,20 @@ function CreateArea(props) {
         event.preventDefault();
     }
 
+    function handleFocus() {
+        setActive(true);
+    }
+
     return (
         <div>
-            <form>
-                <input name="title" placeholder="Title" onChange={handleChange} value={note.title}/>
-                <textarea name="content" placeholder="Take a note..." rows="3" onChange={handleChange} value={note.content}/>
-                <button onClick={handleSubmit}>Add</button>
+            <form className="create-note" onFocus={handleFocus}>
+                {active && <input name="title" placeholder="Title" onChange={handleChange} value={note.title} autoComplete="off" />}
+                <textarea name="content" placeholder="Take a note..." rows={active ? 3 : 1} onChange={handleChange} value={note.content} />
+                <Zoom in={active}>
+                    <Fab onClick={handleSubmit}>
+                        <AddIcon />
+                    </Fab>
+                </Zoom>
             </form>
         </div>
     );
