dd8e481f7d9a2a59a8cbd7dc2728bcf681012371
David Richards, Jr <dbarichardsjr@gmail.com>
2021-04-01 22:37:36 -0400

    Add CreateArea, Adding and Deleting of Notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
+import CreateArea from "./CreateArea";
 import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+    function addNote(note) {
+        setNotes(prevValue => {
+            return [...prevValue, note];
+        });
+    }
+
+    function deleteNote(id) {
+        setNotes(prevValue => {
+            return prevValue.filter((item, index) => {
+                return index !== id;
+            });
+        });
+    }
+
     return (
         <div>
             <Header />
-            {notes.map(note => <Note key={note.id} title={note.title} content={note.content} />)}
+            <CreateArea onAdd={addNote} />
+            {notes.map((note, index) => {
+                return <Note key={index} id={index} title={note.title} content={note.content} delete={deleteNote}/>;
+            })}
             <Footer />
         </div>
     );
